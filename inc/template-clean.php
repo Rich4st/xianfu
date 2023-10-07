a75c72a4bcb4842c5c7d88eaf96b37f1a028873a
Rich4st <zmingcheng204@gmail.com>
2023-10-07 23:13:46 +0800

    新增: 小工具经典模式;

--- inc/template-clean.php
+++ inc/template-clean.php
@@ -10,6 +10,21 @@ class theme_clean
 
   public function __construct()
   {
+    // 关闭顶部导航栏
     add_filter('show_admin_bar', '__return_false');
+
+    // 关闭古腾堡编辑器
+    if (!_capalot('gutenberg_edit')) {
+      add_filter('use_block_editor_for_post', '__return_false');
+      remove_action('wp_enqueue_scripts', 'wp_common_block_scripts_and_styles');
+    }
+
+    // 禁用古腾堡小工具
+    if (!_capalot('gutenberg_widgets', false)) {
+      // Disables the block editor from managing widgets in the Gutenberg plugin.
+      add_filter('gutenberg_use_widgets_block_editor', '__return_false');
+      // Disables the block editor from managing widgets.
+      add_filter('use_widgets_block_editor', '__return_false');
+    }
   }
 }
