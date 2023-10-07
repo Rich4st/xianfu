edfe53cbd28a1569d49c931efb96af9188b5ea68
Rich4st <zmingcheng204@gmail.com>
2023-10-07 22:33:30 +0800

    新增: Jquery;

--- inc/template-assets.php
+++ inc/template-assets.php
@@ -6,10 +6,15 @@
 function load_assets()
 {
 
+  // jquery
+  wp_deregister_script('jquery');
+  wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/jquery.min.js', array(), '3.6.0', false);
+
   // tailwind
   wp_enqueue_style('tailwind', get_template_directory_uri() . '/assets/css/tailwind.css', array(), '1.0.0', 'all');
   // app
   wp_enqueue_style('app', get_template_directory_uri() . '/assets/css/app.css', array(), '1.0.0', 'all');
+  wp_enqueue_script('app', get_template_directory_uri() . '/assets/js/app.js', array(), '1.0.0', true);
   // swiper
   wp_enqueue_style('swiper', get_template_directory_uri() . '/assets/css/swiper-bundle.min.css', array(), '10.1.0');
   wp_enqueue_script('swiper', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array(), '10.1.0', true);
