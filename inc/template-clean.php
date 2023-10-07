9a4a909a3771eb4a665ac27162f10c8852a5dc32
Rich4st <zmingcheng204@gmail.com>
2023-10-07 23:22:16 +0800

    新增: 后台配置仪表盘显示隐藏;

--- inc/template-clean.php
+++ inc/template-clean.php
@@ -3,7 +3,7 @@
 /**
  * 主题性能优化，去除无用功能
  */
-new theme_clean;
+new theme_clean();
 
 class theme_clean
 {
@@ -23,6 +23,34 @@ class theme_clean
     if (!_capalot('gutenberg_widgets', false)) {
       add_filter('gutenberg_use_widgets_block_editor', '__return_false');
       add_filter('use_widgets_block_editor', '__return_false');
+    }
+
+    add_action('admin_menu', array($this, 'admin_render'), 999);
+    add_action('admin_init', array($this, 'admin_init'), 999);
+  }
+
+  public function admin_render()
+  {
+    if (_capalot('remove_admin_menu', true)) {
+      // Remove Dashboard
+      remove_menu_page('index.php');
+      // Remove Dashboard -> Update Core notice
+      remove_submenu_page('index.php', 'update-core.php');
+    }
+    // 移除外观—>主题文件编辑器
+    remove_submenu_page('themes.php', 'theme-editor.php');
+  }
+
+  public function admin_init()
+  {
+
+    //删除仪表盘
+    global $pagenow; // Get current page
+    $redirect = get_admin_url(null, 'edit.php'); // Where to redirect
+
+    if ($pagenow == 'index.php') {
+      wp_redirect($redirect, 301);
+      exit;
     }
   }
 }
