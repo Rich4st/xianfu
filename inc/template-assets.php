0db04dc91662ec88445a13a85d24818cdf935330
Rich4st <zmingcheng204@gmail.com>
2023-10-08 22:52:36 +0800

    新增: iconify;

--- inc/template-assets.php
+++ inc/template-assets.php
@@ -18,5 +18,7 @@ function load_assets()
   // swiper
   wp_enqueue_style('swiper', get_template_directory_uri() . '/assets/css/swiper-bundle.min.css', array(), '10.1.0');
   wp_enqueue_script('swiper', get_template_directory_uri() . '/assets/js/swiper-bundle.min.js', array(), '10.1.0', true);
+  // iconify
+  wp_enqueue_script('icon', get_template_directory_uri() . '/assets/js/iconify.min.js', array(), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'load_assets');
