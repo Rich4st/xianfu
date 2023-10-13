d008fcb96679ae08536615a6d528ebcabcfdf83d
Rich4st <zmingcheng204@gmail.com>
2023-10-13 23:12:38 +0800

    新增: 懒加载配置;

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,20 +3,41 @@ const body = jQuery("body");
 
 let capalot = {
   init: function () {
-    const swiperEl = document.querySelector('.swiper');
-    if (swiperEl)
-      capalot.swiper();
+
+    capalot.swiper();
+    capalot.toggle_dark();
+    capalot.lazyload();
   },
 
   // 轮播初始化
   swiper: function () {
     const swipers = document.querySelectorAll('.mySwiper');
 
+    if (swipers.length === 0) return;
+
     swipers.forEach((el) => {
       new Swiper(el, JSON.parse(el.dataset.config));
       console.log(JSON.parse(el.dataset.config));
     })
   },
+
+  // 深色模式切换
+  toggle_dark: function () {
+    const switcher = document.querySelector('.toggle-dark');
+
+    if (!switcher) return;
+
+    switcher.addEventListener('click', function () {
+      body.toggleClass('dark');
+    })
+  },
+
+  // 懒加载配置
+  lazyload: function () {
+    console.log(LazyLoad);
+    var lazyLoadInstance = new LazyLoad();
+    lazyLoadInstance.update();
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   content: ['./**/*.php', './**/*.js'],
   theme: {
     extend: {
+      backgroundColor: {
+        'dark': '#0c0c0c',
+        'dark-card': '#121212',
+        'primary': '#570df8',
+        'secondary': '#f000b8',
+        'accent': '#1dcdbc',
+        'info': '#3abff8',
+        'success': '#36d399',
+        'error': '#f87272'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+  ],
 }
