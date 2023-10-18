734d55630af16d5ec5a8dcd023fb80a77e251bf1
Rich4st <zmingcheng204@gmail.com>
2023-10-18 22:49:31 +0800

    样式: slider-posts样式优化.

--- template-parts/widget/home/slider-posts.php
+++ template-parts/widget/home/slider-posts.php
@@ -19,15 +19,15 @@ $config = [
   'breakpoints' => [
     320 => [
       'slidesPerView' => 2,
-      'spaceBetween' => 20
+      // 'spaceBetween' => 20
     ],
     768 => [
       'slidesPerView' => 3,
-      'spaceBetween' => 30
+      // 'spaceBetween' => 30
     ],
     1024 => [
       'slidesPerView' => $args['slidesPerView'],
-      'spaceBetween' => $args['spaceBetween']
+      // 'spaceBetween' => $args['spaceBetween']
     ]
   ],
 ]
@@ -50,16 +50,16 @@ $config = [
   <?php endif; ?>
 
   <div class="swiper mySwiper slider-post" data-config='<?php echo json_encode($config); ?>'>
-    <div class="swiper-wrapper">
+    <div class="swiper-wrapper py-0.5">
 
       <?php
       if ($PostData->have_posts()) :
         while ($PostData->have_posts()) : $PostData->the_post() ?>
 
-          <div class="swiper-slide text-white">
+          <div class="swiper-slide border-r  dark:border-r-gray-700 hover:shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] dark:border-b-2 dark:border-b-transparent dark:hover:border-b-pink-500">
             <div class="relative">
               <img src="<?php capalot_get_thumbnail_url(); ?>" class="w-full h-40 object-cover">
-              <div class="text-xs my-2 space-y-1">
+              <div class="text-xs my-2 space-y-1 px-2">
                 <ul class="flex items-center space-x-2">
                   <?php capalot_post_category(2); ?>
                 </ul>
