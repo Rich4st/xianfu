61cef5788588c1ee96b6a6d1d3c245462311db47
Rich4st <zmingcheng204@gmail.com>
2023-11-05 13:13:43 +0800

    style: footer.

--- footer.php
+++ footer.php
@@ -1,7 +1,18 @@
 </main>
 
-<footer class="h-30 border-t mt-8 text-center py-4">
-  footer
+<footer class="mt-2 py-4 bg-gray-800 text-white font-semibold">
+  <ul class="flex flex-col md:flex-row items-center justify-evenly">
+    <li>
+      Copyright © 2023 先富网站 版权所有
+    </li>
+    <li>
+      客服信箱: hi.xianfu@gmail.com
+    </li>
+    <li>
+      <a class="text-pink-500 hover:text-sky-500" href="">服务条款</a>
+      <a class="text-pink-500 hover:text-sky-500" href="">隐私政策</a>
+    </li>
+  </ul>
 </footer>
 
 <?php wp_footer(); ?>
