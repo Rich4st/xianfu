a5b912aec0ceab8258db23801af2a347d6aa34ba
Rich4st <zmingcheng204@gmail.com>
2023-11-09 17:13:50 +0800

    style: single min width.

--- single.php
+++ single.php
@@ -5,7 +5,7 @@ $post_id = get_the_ID();
 ?>
 <?php get_template_part('template-parts/components/single-header') ?>
 <section class="dark:bg-dark flex flex-col lg:flex-row md:gap-8 px-4 lg:px-0 justify-center">
-  <div class="prose prose-xl prose-p:text-base prose-h1:mb-8 prose-li:text-base dark:text-gray-400">
+  <div class="w-full prose prose-xl prose-p:text-base prose-h1:mb-8 prose-li:text-base dark:text-gray-400">
     <div class="my-2">
       <?php
       $categories = get_the_category($post_id);
