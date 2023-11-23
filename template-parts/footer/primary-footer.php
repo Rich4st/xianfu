10a45f2125652d56ebff38e11091430dbfe86dc9
Rich4st <zmingcheng204@gmail.com>
2023-11-23 15:18:42 +0800

    feat: primary footer contact-list.

--- template-parts/footer/primary-footer.php
+++ template-parts/footer/primary-footer.php
@@ -2,30 +2,14 @@
   <ul class="ca-container grid grid-cols-1 lg:grid-cols-3 gap-8 text-center lg:text-left">
     <li>
       <h3 class="mb-2 text-white font-semibold">订阅电子报</h3>
-      <p>你也想成為自由工作者嗎？我會和你分享心路歷程、接案經驗、個人品牌、實用工具、旅行等資訊喔～</p>
+      <p>你也想成为自由工作者吗？我会和你分享心路历程、建站经验、个人品牌、实用工具喔~</p>
     </li>
     <li class="space-y-2">
       <h3 class="text-white font-semibold">关于先富网站</h3>
-      <p>長期分享 WordPress 架站教學、SEO 技巧、網路行銷，幫你提升行銷軟實力，歡迎關注學習！</p>
-      <ul class="flex justify-center lg:justify-start items-center">
-        <li class="text-xl flex gap-4">
-          <a href="https://www.instagram.com/hi.xianfu/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-            <i class="iconify" data-icon="simple-icons:instagram" data-inline="false"></i>
-          </a>
-          <a href="https://twitter.com/hi_xianfu" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-            <i class="iconify" data-icon="akar-icons:x-fill" data-inline="false"></i>
-          </a>
-          <a href="https://line.me/R/ti/p/%40xianfu" target="_blank" rel="noopener noreferrer" aria-label="Line">
-            <i class="iconify" data-icon="simple-icons:line" data-inline="false"></i>
-          </a>
-          <a href="https://wa.me/886912345678" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp">
-            <i class="iconify" data-icon="simple-icons:whatsapp" data-inline="false"></i>
-          </a>
-          <a href="https://www.facebook.com/hi.xianfu" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-            <i class="iconify" data-icon="simple-icons:facebook" data-inline="false"></i>
-          </a>
-        </li>
-      </ul>
+      <p>长期分享 WordPress 建站教学、SEO 技巧、网络营销，帮你提升行销硬实力，欢迎关注学习！</p>
+      <div class="flex justify-center lg:justify-start">
+        <?php get_template_part('template-parts/components/contact-list') ?>
+      </div>
     </li>
     <li>
       <h3 class="mb-2 text-white font-semibold">其他链接</h3>
