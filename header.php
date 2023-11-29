015c5b05104f7c5dae1f1d254a015ec18faac249
Rich4st <zmingcheng204@gmail.com>
2023-11-29 09:03:40 +0800

    feat: GA tag.

--- header.php
+++ header.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html <?php language_attributes();?> class="<?php echo capalot_get_site_theme(); ?>">
+<html <?php language_attributes(); ?> class="<?php echo capalot_get_site_theme(); ?>">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head() ?>
-  <meta name="shenma-site-verification" content="92c25166fdc405fef9ace70831431779_1700898736"/>
+  <meta name="shenma-site-verification" content="92c25166fdc405fef9ace70831431779_1700898736" />
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-C6CEMD4NQM"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-C6CEMD4NQM');
+  </script>
 </head>
 
 <body class="dark:bg-dark">
